d6ae7ce530d0443314e84f4c4f6238cf25151f14
Aryan <aryanx404@gmail.com>
2020-01-21 20:30:15 +0530

    Formatted code using Prettier

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-const Pet = ({name, animal, breed}) => {
-	return React.createElement("div", {}, 
-		[
-		React.createElement("h3", {}, name),
-		React.createElement("p", {}, animal),
-		React.createElement("p", {}, breed)
-		]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h3", {}, name),
+    React.createElement("p", {}, animal),
+    React.createElement("p", {}, breed)
+  ]);
 };
 
 const App = () => {
-	return React.createElement("div", {}, [
-		React.createElement("h1", {}, "Adopt Me!"),
-		React.createElement(Pet, {name: "Calu", animal: "Dog", breed: "Golden Retriever"}),
-		React.createElement(Pet, {name: "Birdie", animal: "Bird", breed: "Humming Bird"}),
-		React.createElement(Pet, {name: "Kitty", animal: "Cat", breed: "Mixed"})
-		]); };  
-		
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Calu",
+      animal: "Dog",
+      breed: "Golden Retriever"
+    }),
+    React.createElement(Pet, {
+      name: "Birdie",
+      animal: "Bird",
+      breed: "Humming Bird"
+    }),
+    React.createElement(Pet, { name: "Kitty", animal: "Cat", breed: "Mixed" })
+  ]);
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
